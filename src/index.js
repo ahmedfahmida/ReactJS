4dade7c1b6b794bdf4538fa1964f2f2a1a9c9b9d
ahmedfahmida <fahmi.ahmed.2008@gmail.com>
2022-05-27 10:47:01 +0600

    New

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import EventHandler from './EventHandler';
-import UseState from './UseState'
-ReactDOM.render( <EventHandler/>, document.getElementById('root')
-  );
-
